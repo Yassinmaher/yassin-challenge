cfd00d61f7277b7d3509e4578bf31f378f08df1d
Yassin Maher <professor.yassin@yahoo.com>
2021-05-26 22:07:25 +0200

    v3.0

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
-    <h1>Yassin Eldeep's <span>Very Hard</span> Challenge</h1>
+    <h1>Yassin Eldeep's <span>Very Hard</span> Challenge &#x1F602;&#x1F602;</h1>
     <div>
-      <button @click="num++">+</button>
+      <button @click="increment(1)">+1</button>
+      <button @click="increment(10)">+10</button>
+      <button @click="increment(20)">+20</button>
+      <button @click="increment(50)">+50</button>
+      <button @click="increment(100)">+100</button>
+      <button @click="increment(200)">+200</button>
       <p>{{ num }}</p>
-      <button @click="num > 0 ? num-- : num">-</button>
+      <button @click="decrement(1)">-1</button>
+      <button @click="decrement(10)">-10</button>
+      <button @click="decrement(20)">-20</button>
+      <button @click="decrement(50)">-50</button>
+      <button @click="decrement(100)">-100</button>
+      <button @click="decrement(200)">-200</button>
     </div>
   </div>
 </template>
@@ -15,6 +25,14 @@ export default {
   data() {
     return {
       num: 0
+    }
+  },
+  methods: {
+    increment(amount) {
+      this.num+=amount
+    },
+    decrement(amount) {
+      this.num > 0 ? this.num-=amount : this.num
     }
   }
 }
@@ -48,10 +66,12 @@ button {
   border: none;
   border-radius: 20px;
   font-size: 20px;
-  width: 50px;
-  height: 40px;
+  /*width: 50px;*/
+  /*height: 40px;*/
   display: table-cell;
   vertical-align: middle;
+  margin-right: 10px;
+  padding: 10px 20px
 }
 
 button:hover {
